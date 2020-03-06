240d5c09a1c4b7f5b5a8c83db589574138fa033d
Yaash Jain <yaash.jain@alumni.ubc.ca>
2020-03-05 21:56:08 -0800

    breaking-the-records problem solution

--- ProblemSolving/breaking-the-records.py
+++ ProblemSolving/breaking-the-records.py
@@ -11,7 +11,22 @@ import sys
 
 # Complete the breakingRecords function below.
 def breakingRecords(scores):
+    records = [0,0]
+    cur_min = 0
+    cur_max = 0
 
+    for i in range(0, len(scores)):
+        if (i == 0):
+            cur_min = scores[i]
+            cur_max = scores[i]
+        elif (scores[i] < cur_min):
+            cur_min = scores[i]
+            records[1] += 1
+        elif (scores[i] > cur_max):
+            cur_max = scores[i]
+            records[0] += 1
+
+    return records
 
 if __name__ == '__main__':
     n = int(input())
